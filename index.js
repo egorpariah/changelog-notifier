7ea872c4f99c36a95f9a2197a527232c9a8ad487
Egor Golikov <elai.asti@gmail.com>
2024-07-03 20:32:25 +0700

    Сделал формирование текста changelog

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const locale = require('./locale.json');
 
 try {
   const prefixes = core.getMultilineInput('prefixes');
   const commits = github.context.payload.commits;
-  for (const commit of commits) {
-    const isCommitMessageHasPrefix = prefixes.some(prefix =>
-      commit.message.includes(prefix)
-    );
-    if (isCommitMessageHasPrefix) {
-      core.info(commit.message);
+  let changelogText = '';
+
+  for (const prefix of prefixes) {
+    changelogText += `*${locale[prefix]}*\n\n`;
+
+    for (const commit of commits) {
+      const isCommitMessageHasPrefix = commit.message.includes(prefix);
+      if (isCommitMessageHasPrefix) {
+        changelogText += `• ${commit.message} (${commit.author.username})\n`;
+      }
     }
+
+    changelogText += '\n\n\n';
   }
+
+  core.info(changelogText);
 } catch (error) {
   core.setFailed(error.message);
 }
